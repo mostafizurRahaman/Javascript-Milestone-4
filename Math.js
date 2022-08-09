27272ec9c4f0af372a71362908c213e72cad20fd
mostafizurRahaman <fahim654326@gmail.com>
2022-08-09 23:20:16 +0600

    math function added

---
+++ Math.js
@@ -0,0 +1,14 @@
+// let number = 2.69; 
+// let number1 = 2.49; 
+// console.log(Math.round(number)); 
+// console.log(Math.round(number1)); 
+// console.log(Math.ceil(number1)); 
+// console.log(Math.floor(number)); 
+
+console.log(Math.round(Math.random()*6)); 
+
+console.log(Math.random());
+console.log(Math.sqrt(16)); 
+console.log(Math.max(20,21, 26)); 
+console.log(Math.min(26,26,28)); 
+console.log(Math.pow(2,2)); 
