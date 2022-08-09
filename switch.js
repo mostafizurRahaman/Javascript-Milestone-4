285ac484c381c72f39a1947ecf13ac57db3b2919
mostafizurRahaman <fahim654326@gmail.com>
2022-08-09 18:15:30 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,20 @@
+let letter = 'h'; 
+switch(letter){
+   case "a": 
+         console.log("vowel"); 
+         break; 
+   case "e": 
+         console.log("vowel"); 
+         break; 
+   case "i":
+         console.log("vowel"); 
+         break; 
+   case "o": 
+         console.log("vowel"); 
+         break; 
+   case "u": 
+         console.log("vowel"); 
+         break;
+   default:  
+         console.log("constant"); 
+}
