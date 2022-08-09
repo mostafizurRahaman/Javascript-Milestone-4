161877dbd3fcdb4e7320adf30ad5ef0135648e6e
mostafizurRahaman <fahim654326@gmail.com>
2022-08-09 23:30:45 +0600

    swamp.js

---
+++ swamp.js
@@ -0,0 +1,30 @@
+let a = 5; 
+let b =10; 
+console.log(a,b); 
+let temp =a; 
+ a = b; 
+ b =temp; 
+console.log(a,b); 
+
+
+let num1 = 10; 
+let num2= 20; 
+console.log(num1, num2); 
+num1 = num1 + num2; 
+num2 = num1-num2; 
+num1 = num1- num2; 
+console.log(num1, num2); 
+
+let name = "Mostafiz"; 
+let name1 = "Fahim"; 
+console.log(name, name1); 
+[name, name1] = [name1, name]; 
+console.log(name, name1); 
+
+let number1 = 40; 
+let number2 = 50; 
+console.log(number1, number2); 
+number1 = number1^number2; 
+number2 = number1^number2; 
+number1 = number1^number2; 
+console.log(number1,number2); 
